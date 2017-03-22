85732a616ba47b53261edd3b5a5779b3be7d5acc
Chknbone <cb@palarran.com>
2017-03-22 22:10:36 +1200

    Google Maps are loading at the users current location
    Added an orange border to the "No Cyclones found" textview in cyclone_list.xml

--- app/src/main/java/com/palarran/cycloops/MainActivity.java
+++ app/src/main/java/com/palarran/cycloops/MainActivity.java
@@ -15,16 +15,22 @@
  */
 package com.palarran.cycloops;
 
+import android.Manifest;
 import android.app.LoaderManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.Loader;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -33,53 +39,59 @@ import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.ArrayList;
 import java.util.List;
 
-//import static com.palarran.cycloops.CycloneMap.START_POINT;
-
-public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<CycloneData>>
-                                                                , OnMapReadyCallback {
+public class MainActivity extends AppCompatActivity implements
+        LoaderManager.LoaderCallbacks<List<CycloneData>>,
+        OnMapReadyCallback,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener {
 
     public static final String LOG_TAG = MainActivity.class.getName();
 
-     //Adapter for the list of Cyclones
+    //Adapter for the list of Cyclones
     private CycloneAdapter mAdapter;
 
-     //URL for Cyclone data from the USGS website data set
+    //URL for JSON Cyclone data from the USGS website data set
     private static final String WUNDERGROUND_CURRENT_HURRICANE_URI
-             = "http://api.wunderground.com/api/95e20de6002dc6f0/currenthurricane/view.json";
+            = "http://api.wunderground.com/api/95e20de6002dc6f0/currenthurricane/view.json";
 
-    /**
-     * Constant value for the Cyclone loader ID. Can be any integer.
-     * This really only comes into play if using multiple loaders.
-     */
+
+    //Constant value for the Cyclone loader ID. Can be any integer.
+    //This really only comes into play if using multiple loaders. Setting to 1
     private static final int CYCLONE_LOADER_ID = 1;
 
-     //TextView that is displayed when the Cyclone list is empty
+    //TextView that is displayed when the Cyclone list is empty
     private TextView mEmptyStateTextView;
 
-     //TextView ths is displayed when there is no available internet/network connection
+    //TextView ths is displayed when there is no available internet/network connection
     private TextView mNoNetworkTextView;
 
     //Defining Google Map objects variables
     GoogleMap mMap;
-    boolean mapReady=false;
-
-    static final CameraPosition START_POINT = CameraPosition.builder()
-            .target(new LatLng(38.1254, -101.1703))
-            .zoom(3)
-            .bearing(359)
-            .tilt(5)
-            .build();
+    boolean mapReady = false;
+    GoogleApiClient mGoogleApiClient;
+    Location mLastLocation;
+    Marker mCurrLocationMarker;
+    LocationRequest mLocationRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -130,10 +142,13 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         // because this activity implements the LoaderCallbacks interface).
         loaderManager.initLoader(CYCLONE_LOADER_ID, null, this);
 
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            checkLocationPermission();
+        }
+        // Obtain the SupportMapFragment and get notified when the map is ready
         //Calling up the map fragment from cyclone_list.xml
         MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.google_map);
         mapFragment.getMapAsync(this);
-
     }
 
     @Override
@@ -214,20 +229,192 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         return super.onOptionsItemSelected(item);
     }
 
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
     @Override
     public void onMapReady(GoogleMap gMap) {
         //Setting mapReady to true
-        mapReady=true;
+        mapReady = true;
 
         //Loading local instance map from Callback
         mMap = gMap;
+        mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
 
-        //Set camera at starting point, high over the middle of the U.S of A.
-        startUpCameraPositon(START_POINT);
+        //checks for permission using the Support library before enabling the My Location layer
+        //Initialize Google Play Services
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)
+                    == PackageManager.PERMISSION_GRANTED) {
+                buildGoogleApiClient();
+                mMap.setMyLocationEnabled(true);
+            }
+        } else {
+            buildGoogleApiClient();
+            mMap.setMyLocationEnabled(true);
+        }
     }
 
-    private void startUpCameraPositon(CameraPosition target) {
-        //Setting position to the target created above
-        mMap.moveCamera(CameraUpdateFactory.newCameraPosition(target));
+    /**
+     * Method GoogleApiClient.Builder is used to configure client.
+     * .addConnectionCallbacks provides callbacks that are called when client is connected or disconnected.
+     * .addOnConnectionFailedListener handles failed connection attempts to service.
+     * .addApi adds the LocationServices API endpoint from Google Play Services.
+     * mGoogleApiClient.connect(): A client must be connected before executing any operation.
+     */
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+        mGoogleApiClient.connect();
+    }
+
+    /**
+     * In a Google Maps App, it is always required to update current location of user at regular
+     * intervals. Also we may want current velocity, altitude etc. These all are covered inside
+     * the location object which can be retrieved using fused location provider.
+     * Fused Location Provider analyses GPS, Cellular and Wi-Fi network location data in order to
+     * provide the highest accuracy data. It uses different device sensors to define if a user is
+     * walking, riding a bicycle, driving a car or just standing in order to adjust the frequency
+     * of location updates. It also helps in android location tracking.
+     * This will be used to get the last updated location.
+     *
+     * @param bundle
+     */
+    @Override
+    public void onConnected(Bundle bundle) {
+
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(1000);
+        mLocationRequest.setFastestInterval(1000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+        }
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    /**
+     * Using this Google predefined function onLocationChanged() that will be called
+     * as soon as user location has changed.
+     *
+     * @param location
+     */
+    @Override
+    public void onLocationChanged(Location location) {
+        mLastLocation = location;
+        if (mCurrLocationMarker != null) {
+            mCurrLocationMarker.remove();
+        }
+
+        //Place current location marker
+        LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(latLng);
+        markerOptions.title("Current Position");
+        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA));
+        mCurrLocationMarker = mMap.addMarker(markerOptions);
+
+        //move map camera
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(11));
+
+        //stop location updates
+        if (mGoogleApiClient != null) {
+            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+        }
+    }
+
+    /**
+     * App will not be granted any permission at installation. Instead, app will ask user for
+     * permission one-by-one at runtime
+     */
+    public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
+
+    public boolean checkLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Asking user if explanation is needed
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+                //Prompt the user once explanation has been shown
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_LOCATION);
+
+
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_LOCATION);
+            }
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    //Handles the result of the permission request by implementing the
+    //ActivityCompat.OnRequestPermissionsResultCallback from the Support library
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted. Do the
+                    // contacts-related task you need to do.
+                    if (ContextCompat.checkSelfPermission(this,
+                            Manifest.permission.ACCESS_FINE_LOCATION)
+                            == PackageManager.PERMISSION_GRANTED) {
+
+                        if (mGoogleApiClient == null) {
+                            buildGoogleApiClient();
+                        }
+                        mMap.setMyLocationEnabled(true);
+                    }
+
+                } else {
+
+                    // Permission denied, Disable the functionality that depends on this permission.
+                    Toast.makeText(this, "permission denied", Toast.LENGTH_LONG).show();
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other permissions this app might request.
+            // You can add here other case statements according to your requirement.
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
     }
 }
